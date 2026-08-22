15c6e7d46ca74f9761ed1ec749f8634217fd2ea3
Noah Gullo <noahgulloingram@gmail.com>
2026-08-22 12:46:49 -0700

    finish 2657. Find the Prefix Common Array of Two Arrays

---
+++ 2657.Find_the_Prefix_Common_Array_of_Two_Arrays/INITIAL.md
@@ -0,0 +1,2 @@
+Initial Thoughts:
+I can loop through one array and push every number into a set. Then for the second array I can check if the elements exist in the set and append to a prefix common array.
---
+++ 2657.Find_the_Prefix_Common_Array_of_Two_Arrays/attempt.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        seen = set()
+        count = 0
+        common = []
+
+        for i in range(len(A)):
+            if A[i] in seen:
+                count += 1
+            seen.add(A[i])
+
+            if B[i] in seen:
+                count += 1
+            seen.add(B[i])
+
+            common.append(count)
+
+        return common
